1d36c93f858349043974709032be1337118bece4
MrJackphil <geimerus@gmail.com>
2024-08-17 20:21:18 +0400

    feat: kill on touch

---
+++ components/composed/KillOnTouch.gd
@@ -0,0 +1,17 @@
+extends Area3D
+class_name KillOnTouch
+
+@export var DAMAGE = null
+
+func _ready() -> void:
+	area_entered.connect(_collide)
+
+func _collide(collider: Node3D) -> void:
+	if collider.owner.is_in_group("player"):
+		var hp_component: HealthComponent = collider.find_child("HealthComponent")
+		if not hp_component: return
+		
+		if not DAMAGE:
+			hp_component.hp = 0
+		else:
+			hp_component.hp -= DAMAGE
