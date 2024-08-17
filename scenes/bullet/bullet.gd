615f44c3b467532e483453641d108d41f8d4ba7b
MrJackphil <geimerus@gmail.com>
2024-08-17 19:22:36 +0400

    feat: enemy_bullet

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -4,12 +4,20 @@ extends Area3D
 @export var BOUNCE_COUNT := 1
 
 func _ready() -> void:
-	var die_timer: SceneTreeTimer = get_tree().create_timer(3.0)
-	die_timer.timeout.connect(owner.queue_free)
+	pass
+	#var die_timer: SceneTreeTimer = get_tree().create_timer(3.0)
+	#die_timer.timeout.connect(owner.queue_free)
 	area_entered.connect(_collide)
 
 func _collide(collider: Node3D) -> void:
 	var child := collider.get_parent().get_children()
+	
+	if collider.owner.is_in_group("player"):
+		var hp_component: HealthComponent = collider.find_child("HealthComponent")
+		if not hp_component: return
+		
+		hp_component.hp -= 10
+		
 	for c in child:
 		if c.has_method("collide"):
 			c.collide()
