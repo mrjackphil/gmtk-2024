1f3e32032f8919b84dca399297cb8a68a94ca6b0
MrJackphil <geimerus@gmail.com>
2024-08-17 17:18:31 +0400

    fix: remove ridigbody destroy for bullet

--- scenes/bullet/bullet_area.gd
+++ scenes/bullet/bullet_area.gd
@@ -2,7 +2,7 @@ extends RigidBody3D
 
 func _ready() -> void:
 	linear_velocity = global_transform.basis.z * -1 * 30
-	body_entered.connect(_destroy)
+	#body_entered.connect(_destroy)
 
 func _destroy(_arg: Variant) -> void:
 	queue_free()
