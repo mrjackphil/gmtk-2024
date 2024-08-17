019d589a5cd6c81c3dfb6afe88b300908662ac96
MrJackphil <geimerus@gmail.com>
2024-08-17 21:06:18 +0400

    feat: add destruction scale treshold

--- components/composed/Destructable.gd
+++ components/composed/Destructable.gd
@@ -1,6 +1,8 @@
 extends Area3D
 class_name Destructable
 
+@export var SCALE_TRESHOLD := 1.2
+
 func _ready() -> void:
 	area_entered.connect(_destruction)
 	
@@ -10,13 +12,13 @@ func _destruction(area: Area3D) -> void:
 		queue_free()
 
 func _check_scale(area: Node3D) -> bool:
-	if area.scale.x > 1: 
+	if area.scale.x > SCALE_TRESHOLD: 
 		return true
 		
-	if area.scale.y > 1: 
+	if area.scale.y > SCALE_TRESHOLD: 
 		return true
 	
-	if area.scale.z > 1: 
+	if area.scale.z > SCALE_TRESHOLD: 
 		return true
 	
 	return false
