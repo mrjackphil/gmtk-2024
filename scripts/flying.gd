329f1e9168f14e8c9a26ccc1aac76ad94a63824b
lucius <flyingrissole@gmail.com>
2024-08-17 22:27:19 +0300

    feat: add flying mob

--- scripts/flying.gd
+++ scripts/flying.gd
@@ -1,9 +1,14 @@
 extends Enemy
 
 @export var looses_player: bool = false
+@export var speed_modificator: float = 0.0
+
+@onready var _behavior_component: EnemyBehaviorComponent = $Components/EnemyBehaviorComponent
+@onready var _movement_component: EnemyMovementComponent = $Components/EnemyMovementComponent
 
 func _ready():
-	$Components/EnemyBehaviorComponent.looses_player = looses_player
+	_behavior_component.looses_player = looses_player
+	_movement_component.speed_modificator = speed_modificator
 
 func _physics_process(delta):
 	pass
