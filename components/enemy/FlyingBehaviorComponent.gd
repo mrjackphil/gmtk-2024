45440cdcf2bb1b964bc03c271a3790c47c253ab7
lucius <flyingrissole@gmail.com>
2024-08-17 20:57:07 +0300

    add flying mob

---
+++ components/enemy/FlyingBehaviorComponent.gd
@@ -0,0 +1,9 @@
+extends EnemyBehaviorComponent
+class_name FlyingBehaviorComponent
+
+
+func _do_triggered_state(delta: float, enemy_position: Vector3):
+	var target_position: Vector3 = _detected_body.global_transform.origin
+	rotation_component.set_rotation_vector(target_position)
+	if movement_component:
+		movement_component.set_destination(Vector3(target_position.x, target_position.y + 1.7, target_position.z))
---
+++ scripts/flying.gd
@@ -0,0 +1,4 @@
+extends Enemy
+
+func _physics_process(delta):
+	pass
