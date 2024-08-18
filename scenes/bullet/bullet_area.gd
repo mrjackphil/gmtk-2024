a6d14f169e0951473ad7d01a688ffb7a868b2d62
MrJackphil <geimerus@gmail.com>
2024-08-18 14:26:41 +0400

    feat: bullet destruction on collision

--- scenes/bullet/bullet_area.gd
+++ scenes/bullet/bullet_area.gd
@@ -1,13 +1,21 @@
 extends RigidBody3D
 
-@export var SPEED := 60
+@export var SPEED := 100
+var to_kill := false
+var timer_to_destroy := 5
 
 func _ready() -> void:
 	linear_velocity = global_transform.basis.z * -1 * SPEED
-	body_entered.connect(_stop)
-	
+	body_entered.connect(_destroy)
+
+func _process(_delta: float) -> void:
+	if to_kill:
+		timer_to_destroy -= 1
+	if timer_to_destroy <= 0:
+		queue_free()
+
 func _stop(_arg: Variant) -> void:
 	freeze = true
-	
+
 func _destroy(_arg: Variant) -> void:
-	queue_free()
+	if not to_kill: to_kill = true
