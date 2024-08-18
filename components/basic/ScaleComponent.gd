d849c97f4184a3598cb8dea1f57cd8a63b64a886
MrJackphil <geimerus@gmail.com>
2024-08-18 14:59:54 +0400

    feat: control scale component target

--- components/basic/ScaleComponent.gd
+++ components/basic/ScaleComponent.gd
@@ -15,14 +15,21 @@ var _timeout := 0.0
 var _scaling_up := false
 var target_scale := Vector3.ONE
 
+@export var target: Node3D
 @onready var parent: Node3D = get_parent()
+
+func _get_collider() -> Node3D:
+	if target:
+		return target
+		
+	return parent
 
 func collide() -> void:
 	_scaling_up = true
 	_timeout = SHRINK_TIMEOUT
-	target_scale.x = clamp(parent.scale.x + SCALE_VELOCITY.x, MIN_SCALE, MAX_SCALE)
-	target_scale.y = clamp(parent.scale.y + SCALE_VELOCITY.y, MIN_SCALE, MAX_SCALE)
-	target_scale.z = clamp(parent.scale.z + SCALE_VELOCITY.z, MIN_SCALE, MAX_SCALE)
+	target_scale.x = clamp(_get_collider().scale.x + SCALE_VELOCITY.x, MIN_SCALE, MAX_SCALE)
+	target_scale.y = clamp(_get_collider().scale.y + SCALE_VELOCITY.y, MIN_SCALE, MAX_SCALE)
+	target_scale.z = clamp(_get_collider().scale.z + SCALE_VELOCITY.z, MIN_SCALE, MAX_SCALE)
 
 func _vec_more_each(vec1: Vector3, vec2: Vector3) -> bool:
 	var x := vec1.x > vec2.x
@@ -32,15 +39,15 @@ func _vec_more_each(vec1: Vector3, vec2: Vector3) -> bool:
 	return x and y and z
 
 func _physics_process(delta: float) -> void:
-	if _vec_more_each(parent.scale, target_scale * 0.95):
+	if _vec_more_each(_get_collider().scale, target_scale * 0.95):
 		_scaling_up = false
 
 	if _scaling_up:
-		parent.scale = lerp(parent.scale, target_scale, SCALE_SLOWNESS * delta)
+		_get_collider().scale = lerp(_get_collider().scale, target_scale, SCALE_SLOWNESS * delta)
 
 	if  not _scaling_up and _timeout > 0.0:
 		_timeout -= 1.0
 		return
 
 	if not _scaling_up and _timeout <= 0.0:
-		parent.scale = lerp(parent.scale, Vector3.ONE, SHRINK_SLOWNESS * delta)
+		_get_collider().scale = lerp(_get_collider().scale, Vector3.ONE, SHRINK_SLOWNESS * delta)
